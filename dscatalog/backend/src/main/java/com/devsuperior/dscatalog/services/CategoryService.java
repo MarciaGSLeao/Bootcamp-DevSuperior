551e09ce556163ca8cb593910e51d7170de107bb
Marcia Gameleira <gameleira.mag@hotmail.com>
2025-08-12 06:44:22 -0300

    PageRequest: paginação para listar dados.

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -8,6 +7,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,9 +27,9 @@ public class CategoryService {
 //	private CategoryDTO categoryDTO;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> listCategories = repository.findAll();
-		return listCategories.stream().map(c -> new CategoryDTO(c)).toList();
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> listCategories = repository.findAll(pageRequest);
+		return listCategories.map(c -> new CategoryDTO(c));
 	}
 	
 //		List<CategoryDTO> listDto = new ArrayList<>();
