ad03c1cbad22505ff9df8cdf6225849c369f08f9
Marcia Gameleira <gameleira.mag@hotmail.com>
2025-08-12 06:12:51 -0300

    Dados para auditoria.
    Inserção de atributos createdAt e updatedAt para registrar timestamp de últimas atualizações.

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,9 +1,14 @@
 package com.devsuperior.dscatalog.entities;
 
+import java.time.Instant;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -14,6 +19,12 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category(){
     }
@@ -38,8 +49,26 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public Instant getCreatedAt() {
+		return createdAt;
+	}
 
-    @Override
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+	
+	@Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
